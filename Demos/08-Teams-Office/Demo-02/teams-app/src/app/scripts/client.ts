c32f653dac2652ebad750287b52e9c220f7762a0
Alexander Pajer <alexander.pajer@integrations.at>
2021-03-18 08:31:19 +0100

    tab

--- Demos/08-Teams-Office/Demo-02/teams-app/src/app/scripts/client.ts
+++
@@ -1,4 +0,0 @@
-// Automatically added for the firstTeamsTab tab
-export * from "./firstTeamsTab/FirstTeamsTab";
-export * from "./firstTeamsTab/FirstTeamsTabConfig";
-export * from "./firstTeamsTab/FirstTeamsTabRemove";
