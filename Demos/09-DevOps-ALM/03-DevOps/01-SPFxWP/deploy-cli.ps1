6aab16c3039ec12251c27e305a3d46553b13b7cd
Alexander Pajer <alexander.pajer@integrations.at>
2021-03-18 09:51:24 +0100

    proj fix

--- Demos/09-DevOps-ALM/03-DevOps/01-SPFxWP/deploy-cli.ps1
+++ Demos/09-DevOps-ALM/03-DevOps/01-SPFxWP/deploy-cli.ps1
@@ -1,6 +1,6 @@
 # Make sure you have logged in to M365
 
-$siteUrl = "https://integrationsonline.sharepoint.com/sites/M365ClientDevFundamentals"
+$siteUrl = "https://integrationsonline.sharepoint.com/sites/M365Dev"
 $appId = m365 spo app add --filePath ./spfx-alm/sharepoint/solution/sp-fx-alm.sppkg --overwrite 
 m365 spo app deploy --id $appId --skipFeatureDeployment
 m365 spo app install --id $appId --siteUrl $siteUrl
