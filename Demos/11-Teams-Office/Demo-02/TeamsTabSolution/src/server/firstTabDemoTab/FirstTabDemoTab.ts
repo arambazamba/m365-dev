d4012cf70580c3a67a69e1bb3890b8efb14cc9ba
Alexander Pajer <alexander.pajer@integrations.at>
2022-04-08 07:08:11 +0200

    teams-deleted

--- Demos/11-Teams-Office/Demo-02/TeamsTabSolution/src/server/firstTabDemoTab/FirstTabDemoTab.ts
+++
@@ -1,8 +0,0 @@
-import { PreventIframe } from "express-msteams-host";
-
-/**
- * Used as place holder for the decorators
- */
-@PreventIframe("/firstTabDemoTab/index.html")
-export class FirstTabDemoTab {
-}
